e4e09ad68172bff8bc675d4f2861415be22e6fce
Istvan Szalontai <istvan.szalontai12@gmail.com>
2018-07-01 22:49:46 +0200

    almost ready

--- jmlpgame/Invaders/src/invaders/Invaders.java
+++ jmlpgame/Invaders/src/invaders/Invaders.java
@@ -5,18 +5,37 @@
  */
 package invaders;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
 /**
  *
  * @author istvanszalontai
  */
-public class Invaders {
+public class Invaders extends JFrame {
 
+    public Invaders(){
+        initUI();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello vilag");
+        //System.out.println("Hello vilag");
+        EventQueue.invokeLater(()->{
+            Invaders invaders = new Invaders();
+            invaders.setVisible(true);
+        });
+    }
+
+    private void initUI() {
+        add(new Board());
+        setSize(600,600);
+        setTitle("Invaders");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
