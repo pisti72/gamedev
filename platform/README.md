e430814ac840cd35b5227eb9e5272cc51dce094f
Istvan Szalontai <istvan.szalontai12@gmail.com>
2019-03-11 08:38:35 +0100

    platform added

---
+++ platform/README.md
@@ -0,0 +1,19 @@
+# Platform game 2D
+
+2D tile based platform game with level editor. Engine should be in different folder.
+
+# How to play
+
+Go  and jump
+
+# Controls
+
+Go left and right and jump.
+
+# Play it here
+
+https://pisti72.github.io/gamedev/platform/platform.html
+
+
+
+
---
+++ platform/platform.js
@@ -0,0 +1,43 @@
+console.log('Started');
+//document.onmousemove = onMouseMove;
+//document.onmousedown = onMousePressed;
+//document.onmouseup = onMouseReleased;
+document.body.onload = function (e) {
+    init();
+    gameLoop();
+}
+var img_pointer = new Image();
+var img_tiles = new Image();
+var mouse = {x:0, y:0}
+var ctx = f('display').getContext('2d');
+var w = f('display').width = document.body.clientWidth;
+var h = f('display').height = document.body.clientHeight;
+console.log('Display: ' + w + ' x ' + h);
+
+function init() {
+    img_pointer.src = 'gfx/hand.png';
+	img_tiles.src = 'gfx/tiles.png';
+}
+
+function gameloop() {
+    requestAnimationFrame(gameloop);
+}
+
+function onMouseMove(e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+}
+
+//cursor
+function drawCursor() {
+    ctx.drawImage(img_tiles, 0, 96, 64, 32, Math.floor(mouse.x / tilew) * tilew, Math.floor(mouse.y / tileh) * tileh - shake, 64, 32);
+}
+
+function drawTile(n, i, z) {
+    ctx.drawImage(img_tiles, n * 64, 0, 64, 128, (i % period) * tilew, Math.floor(i / period) * tileh - 96 - z, 64, 128);
+}
+
+function f(i) {
+    return document.getElementById(i);
+}
+
