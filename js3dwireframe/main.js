3452ab1f50e3f1c859f28abbc603e5c73cf6e771
Istvan Szalontai <istvan.szalontai12@gmail.com>
2020-01-16 17:06:31 +0100

    not yet drawing

--- js3dwireframe/main.js
+++ js3dwireframe/main.js
@@ -1,23 +1,42 @@
 console.log('main loaded');
 //document.onmousemove = onMouseMove;
-document.onkeydown = function(e) {
-    if(e.key == 'w') {
-        console.log('w pressed');
+document.onkeydown = function (e) {
+    if (e.key == 'w') {
+        Wire3d.playerForward();
+    } else if (e.key == 's') {
+        Wire3d.playerBackward();
+    }
+    if (e.key == 'a') {
+        Wire3d.playerLeft();
+    } else if (e.key == 'd') {
+        Wire3d.playerRight();
+    }
+}
+document.onkeyup = function (e) {
+    if (e.key == 'w' || e.key == 's') {
+        Wire3d.playerNotForward();
+    }
+    if (e.key == 'a' || e.key == 'd') {
+        Wire3d.playerDoNotRot();
     }
 }
 document.body.onload = function (e) {
     Wire3d.init();
     init();
-    update();
+    //update();
+    setInterval(() => {
+        update();
+    }, 50);
 }
 
 function init() {
-    Wire3d.addCube(0,0,0);
-    
+    Wire3d.addCube(10, 0, 0, 0);
+
 }
 
 function update() {
     Wire3d.render();
-    Wire3d.debug('speed: ' + Wire3d.getPlayerSpeed());
-    requestAnimationFrame(update);
+    Wire3d.debug('speed: ' + Wire3d.getPlayerSpeed(), 40);
+    Wire3d.debug('rotation: ' + Wire3d.getPlayerRot(), 80);
+    //requestAnimationFrame(update);
 }
--- js3dwireframe/wire3d.js
+++ js3dwireframe/wire3d.js
@@ -2,54 +2,165 @@ console.log('wire loaded');
 
 var Wire3d = {
     ctx: {},
+    objects: [],
+    meshes: [],
     w: 0,
     h: 0,
     player: {
         x: 0,
         y: 0,
-        acc: 0,
         speed: 0,
-        rot: 0
+        rot: 0,
+        speedOfRot: 0,
+        isForward: false,
+        isBackward: false,
+        isRotRight: false,
+        isRotLeft: false
     },
     init: function () {
         var canvas = document.getElementsByTagName('canvas')[0];
         this.ctx = canvas.getContext('2d');
         this.w = canvas.width = document.body.clientWidth;
         this.h = canvas.height = document.body.clientHeight;
+        this.meshes = [];
+        //create cube
+        this.addMesh('cube', [
+            -1, -1, -1,
+            1, -1, -1,
+            1, 1, -1,
+            -1, 1, -1,
+            -1, -1, 1,
+            1, -1, 1,
+            1, 1, 1,
+            -1, 1, 1
+        ], [
+            0, 1,
+            1, 2,
+            2, 3,
+            3, 0,
+            0, 4,
+            1, 5,
+            2, 6,
+            3, 7,
+            4, 5,
+            4, 6,
+            6, 7,
+            7, 4]);
     },
-    addCube: function (x, y, z) {
-        console.log('cube added');
+    addObject: function (name, scale, x, y, z) {
+        var object = {
+            name: name,
+            scale: scale,
+            x: x,
+            y: y,
+            z: z
+        }
+        this.objects.push(object);
     },
-    debug: function (n) {
+    addMesh: function (name, points, edges) {
+        var p = [];
+        var e = [];
+        for (var i = 0; i < Math.floor(points.length / 3); i += 3) {
+            p.push(
+                {
+                    x: points[i],
+                    y: points[i + 1],
+                    z: points[i + 2]
+                });
+        }
+        for (var i = 0; i < Math.floor(edges.length / 2); i += 2) {
+            p.push(
+                {
+                    a: edges[i],
+                    b: edges[i + 1]
+                });
+        }
+        var mesh = {
+            name: name,
+            points: p,
+            edges: e
+        }
+        this.meshes.push(mesh);
+    },
+    getMeshByName: function (name) {
+        for (var i = 0; i < this.meshes.length; i++) {
+            if (name == this.meshes[i].name) {
+                return this.meshes[i];
+            }
+        }
+    },
+    addCube: function (scale, x, y, z) {
+        this.addObject('cube', scale, x, y, z);
+    },
+    debug: function (n, line) {
         this.ctx.fillStyle = '#000';
         this.ctx.font = "30px Arial";
-        this.ctx.fillText(n, 10, 40);
+        this.ctx.fillText(n, 10, line);
     },
     render: function () {
-        this.ctx.fillStyle = '#eee';
+        this.ctx.fillStyle = '#EEE';
         this.ctx.fillRect(0, 0, this.w, this.h);
         this.ctx.fillStyle = '#000';
         this.playerUpdate();
-        console.log('rendered');
+        for (var i = 0; i < this.objects.length; i++) {
+            var object = this.objects[i];
+            var mesh = this.getMeshByName(object.name);
+            //look above
+            for (var j = 0; j < mesh.points.length; j++) {
+                var point
+            }
+        }
     },
-    playerUpdate: function(){
+    playerUpdate: function () {
         this.player.x += Math.cos(this.player.rot) * this.player.speed;
         this.player.y += Math.sin(this.player.rot) * this.player.speed;
-        this.player.speed *= .99;
+        this.player.rot += this.player.speedOfRot;
+        this.player.speed *= .95;
+        this.player.speedOfRot *= .95;
+        if (Math.abs(this.player.speed) < 0.1) {
+            this.player.speed = 0;
+        }
+        if (Math.abs(this.player.speedOfRot) < 0.01) {
+            this.player.speedOfRot = 0;
+        }
+        if (this.player.isForward) {
+            this.player.speed += 1;
+        } else if (this.player.isBackward) {
+            this.player.speed -= 1;
+        }
+        if (this.player.isRotLeft) {
+            this.player.speedOfRot += .1;
+        } else if (this.player.isRotRight) {
+            this.player.speedOfRot -= .1;
+        }
     },
-    playerForward: function(){
-        this.player.speed += 1;
+    playerForward: function () {
+        this.player.isForward = true;
     },
-    playerBackward: function(){
-
+    playerBackward: function () {
+        this.player.isBackward = true;
     },
-    playerLeft: function(){
-
+    playerNotForward: function () {
+        this.player.isForward = false;
+        this.player.isBackward = false;
     },
-    playerRight: function(){
-
+    playerLeft: function () {
+        this.player.isRotLeft = true;
     },
-    getPlayerSpeed:function(){
+    playerRight: function () {
+        this.player.isRotRight = true;
+    },
+    playerDoNotRot: function () {
+        this.player.isRotLeft = false;
+        this.player.isRotRight = false;
+    },
+    getPlayerSpeed: function () {
         return this.player.speed;
+    },
+    getPlayerRot: function () {
+        return this.player.rot;
+    },
+    getPlayer: function () {
+        return this.player;
     }
 }
