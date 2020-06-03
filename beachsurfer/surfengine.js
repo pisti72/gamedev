84b400b450f1448385f9b0fd1fbb8fa94bffaba2
Istvan Szalontai <istvan.szalontai12@gmail.com>
2020-06-03 20:01:45 +0200

    moves

--- beachsurfer/surfengine.js
+++ beachsurfer/surfengine.js
@@ -3,29 +3,36 @@ Surf = {
         text: '',
         on: true
     },
+    paper: '#66f',
+    image: {},
+    tiles: [],
     pressed: {
-        leftKey: ['a','j'],
-        righKey: ['d','l']
+        leftKeys: ['a', 'j', 'ArrowLeft'],
+        righKeys: ['d', 'l', 'ArrowRight'],
+        upKeys: ['w', 'i', 'ArrowUp'],
+        downKeys: ['s', 'k', 'ArrowDown']
     },
-    actors:[],
+    actors: [],
     ctx: {},
     size: {
-        heightInPixel: 96
+        heightInPixel: 128
     },
     init: function () {
         this.createCanvas();
-        this.createDebug();
+        this.image = document.getElementsByTagName('IMG')[0];
         document.addEventListener("keydown", function (e) {
-            if (e.key == pressed.leftKey) {
-                pressed.left = true;
-            } else if (e.key == pressed.rightKey) {
-                pressed.right = true;
+            if (Surf.pressed.leftKeys.includes(e.key)) {
+                Surf.pressed.left = true;
+            } else if (Surf.pressed.rightKeys.includes(e.key)) {
+                Surf.pressed.right = true;
+            }
+            if (Surf.pressed.upKeys.includes(e.key)) {
+                Surf.pressed.up = true;
+            } else if (Surf.pressed.downKeys.includes(e.key)) {
+                Surf.pressed.down = true;
             }
         });
-        this.print('Hello');
-        this.print('Szabi');
-        this.drawDebug();
-        this.drawRect();
+        this.fillAll();
     },
     createCanvas: function () {
         var canvas = document.createElement('canvas');
@@ -39,24 +46,74 @@ Surf = {
         this.size.height = canvas.height = window.innerHeight;
         this.size.pixel = Math.floor(this.size.height / this.size.heightInPixel);
         //this.size.tile = this.size.pixel * 8;
-        console.log(this.size.pixel);
-    },
-    createDebug: function () {
-        if (this.debug.on) {
-            this.debug.elem = document.createElement('div');
-            document.body.appendChild(this.debug.elem);
-        }
+        //console.log(this.size.pixel);
+
     },
     print: function (line) {
-        this.debug.text += line + '<br>';
+        this.debug.text += line + ' - ';
+    },
+    addActor: function (obj) {
+        var tile = this.getTile(obj.tile);
+        obj.tile = tile;
+        this.actors.push(obj);
+    },
+    update: function () {
+        this.clearActors();
+        this.print('Hello');
+        this.print('Szabi');
+        this.moveActors();
+        //this.drawRect();
+        this.drawActors();
+        this.drawDebug();
+    },
+    moveActors: function () {
+        for (var i = 0; i < this.actors.length; i++) {
+            var actor = this.actors[i];
+            actor.x += actor.xd;
+            actor.y += actor.yd;
+        }
+    },
+    drawActors: function () {
+        this.ctx.imageSmoothingEnabled = false;
+        for (var i = 0; i < this.actors.length; i++) {
+            var actor = this.actors[i];
+            this.ctx.drawImage(this.image,
+                actor.tile.x, actor.tile.y,
+                actor.tile.width, actor.tile.height,
+                actor.x, actor.y,
+                actor.tile.width * this.size.pixel, actor.tile.height * this.size.pixel);
+        }
+    },
+    clearActors: function () {
+        this.ctx.fillStyle = this.paper;
+        for (var i = 0; i < this.actors.length; i++) {
+            var actor = this.actors[i];
+            this.ctx.fillRect(
+                actor.x, actor.y,
+                actor.tile.width * this.size.pixel, actor.tile.height * this.size.pixel);
+        }
+    },
+    getTile: function (id) {
+        for (var i = 0; i < this.tiles.length; i++) {
+            var tile = this.tiles[i];
+            if (tile.id == id) return tile;
+        }
+        return false;
     },
     drawDebug: function () {
         if (this.debug.on) {
-            this.debug.elem.innerHTML = this.debug.text;
+            this.ctx.font = "16px Arial";
+            this.ctx.stroke =
+                this.ctx.fillStyle = '#444';
+            this.ctx.fillText(this.debug.text, 10, 20);
             this.debug.text = '';
         }
     },
-    drawRect: function (){
-        this.ctx.fillRect(0,0,100,100);
+    drawRect: function () {
+        this.ctx.fillRect(0, 0, 100, 100);
+    },
+    fillAll: function () {
+        this.ctx.fillStyle = this.paper;
+        this.ctx.fillRect(0, 0, this.size.width, this.size.height);
     }
 }
