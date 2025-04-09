8ec711f68669b702eae7e4de3e0484e1290c1ab8
Istvan Szalontai <istvan.szalontai12@gmail.com>
2025-04-09 10:56:50 +0200

    more levels added

---
+++ js_tutorial/nongram/nonogram/levels.lua
@@ -0,0 +1,58 @@
+levels = {
+    {
+        id = 1,
+        name = "smiley",
+        left = 1,
+        top = 1,
+        width = 12,
+        height = 12
+    },
+    {
+        id = 2,
+        name = "say something",
+        left = 16,
+        top = 4,
+        width = 13,
+        height = 11
+    },
+    {
+        id = 3,
+        name = "suitcase",
+        left = 3,
+        top = 17,
+        width = 17,
+        height = 17
+    },
+    {
+        id = 4,
+        name = "casette",
+        left = 39,
+        top = 1,
+        width = 18,
+        height = 14
+    },
+    {
+        id = 5,
+        name = "floppy",
+        left = 20,
+        top = 28,
+        width = 18,
+        height = 18
+    },
+    {
+        id = 6,
+        name = "car",
+        left = 19,
+        top = 16,
+        width = 18,
+        height = 12
+    },
+    {
+        id = 7,
+        name = "skull key",
+        left = 29,
+        top = 0,
+        width = 10,
+        height = 17
+    }
+}
--- js_tutorial/nongram/nonogram/main.lua
+++ js_tutorial/nongram/nonogram/main.lua
@@ -1,7 +1,14 @@
+require("levels")
+local current_level = {}
+
 function love.load()
     NONE = 1
-    GRID = 26
+    BLACK = 2
+    CROSS = 3
+    GRID = 24
     MARGIN = 2
+    YELLOW = { 0.8, 0.8, 0.5, 0.5 }
+    RED = { 0.8, 0, 0 }
     grid = GRID + MARGIN
     title = "Nonogram"
     width, height = 1024, 600
@@ -11,13 +18,47 @@ function love.load()
     imagedata = love.image.newImageData('pictures.png')
     image     = love.graphics.newImage(imagedata)
     font      = love.graphics.newFont(GRID)
+
     love.graphics.setFont(font)
-    offset_x = math.floor((width - 14 * grid) / 2)
-    offset_y = math.floor((height - 13 * grid) / 2)
+
+    is_mouse_down = false
+    mouse_state = NONE
     tiles = {}
-    for j = 0, 13 do
-        for i = 0, 14 do
-            local r, g, b = imagedata:getPixel(i, j)
+    load_level_by_id(6)
+    --counting rows
+
+    --counting columns
+end
+
+function load_level_by_id(id)
+    local level = get_level_by_id(id)
+    current_level = {
+        name = level.name,
+        left = level.left,
+        top = level.top,
+        width = level.width - 1,
+        height = level.height - 1,
+        offset_x = (width - level.width * grid) / 2,
+        offset_y = (height - level.height * grid) / 2,
+    }
+    copy_pixels()
+    counting_rows()
+    counting_columns()
+end
+
+function get_level_by_id(id)
+    for i = 1, #levels do
+        local level = levels[i]
+        if level.id == id then
+            return level
+        end
+    end
+end
+
+function copy_pixels()
+    for j = 0, current_level.height do
+        for i =0, current_level.width do
+            local r, g, b = imagedata:getPixel(i +  current_level.left, j + current_level.top)
             local color = 0
             if r < 0.5 then
                 color = 1
@@ -32,11 +73,13 @@ function love.load()
             table.insert(tiles, tile)
         end
     end
-    --counting rows
-    for j = 0, 13 do
+end
+
+function counting_rows()
+    for j = 0, current_level.height do
         local counter = 0
         local n = 0
-        for i = 0, 14 do
+        for i = 0, current_level.width do
             local tile = get_tile(i, j)
             if tile.value == 1 then
                 counter = counter + 1
@@ -46,7 +89,7 @@ function love.load()
                     local digit = {
                         img = 2,
                         value = counter,
-                        x = 14 + n,
+                        x = current_level.width + n,
                         y = j,
                         state = NONE
                     }
@@ -56,11 +99,13 @@ function love.load()
             end
         end
     end
-    --counting columns
-    for i = 0, 14 do
+end
+
+function counting_columns()
+    for i = 0, current_level.width do
         local counter = 0
         local n = 0
-        for j = 13, 0, -1 do
+        for j = current_level.height, 0, -1 do
             local tile = get_tile(i, j)
             if tile.value == 1 then
                 counter = counter + 1
@@ -91,6 +136,12 @@ function get_tile(x, y)
     end
 end
 
+function get_tile_by_coord(x, y)
+    local x = x * grid + current_level.offset_x
+    local y = y * grid + current_level.offset_y
+    get_tile(x, y)
+end
+
 function update()
 
 end
@@ -103,20 +154,43 @@ function love.draw()
     --love.graphics.print("Tiles: " .. #tiles, 400, 320)
     for i = 1, #tiles do
         local tile = tiles[i]
-        local x = tile.x * grid + offset_x
-        local y = tile.y * grid + offset_y
+        local x = tile.x * grid + current_level.offset_x
+        local y = tile.y * grid + current_level.offset_y
         if tile.img == 1 then
-            if tile.value == 1 then
-                love.graphics.setColor(0, 0, 0)
-            else
+            if tile.state == NONE then
                 love.graphics.setColor(0.5, 0.5, 0.5)
+                love.graphics.rectangle("fill", x, y, GRID, GRID)
+            elseif tile.state == BLACK then
+                love.graphics.setColor(0, 0, 0)
+                love.graphics.rectangle("fill", x, y, GRID, GRID)
+            else
+                love.graphics.setColor(RED)
+                love.graphics.print("X", x, y)
             end
+
             local xm = love.mouse.getX()
             local ym = love.mouse.getY()
             if xm > x and xm <= x + grid and ym > y and ym <= y + grid then
-                love.graphics.setColor(0.7, 0.7, 0.5)
+                love.graphics.setColor(YELLOW)
+                love.graphics.rectangle("fill", x, y, GRID, GRID)
+                if love.mouse.isDown(1) then
+                    if not is_mouse_down then
+                        mouse_state = tile.state
+                        is_mouse_down = true
+                    end
+                    if mouse_state == tile.state then
+                        if tile.state == NONE then
+                            tile.state = BLACK
+                        elseif tile.state == BLACK then
+                            tile.state = CROSS
+                        else
+                            tile.state = NONE
+                        end
+                    end
+                else
+                    is_mouse_down = false
+                end
             end
-            love.graphics.rectangle("fill", x, y, GRID, GRID)
         elseif tile.img == 2 then
             love.graphics.setColor(0, 0, 0)
             love.graphics.print(tile.value, x, y)
@@ -128,4 +202,8 @@ function love.keypressed(key, scancode, isrepeat)
     if key == "escape" then
         love.event.quit()
     end
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    local tile = get_tile_by_coord(x, y)
 end
