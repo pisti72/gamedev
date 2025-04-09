9c509e1a05765cc930c3a5127b97248ff4498541
Istvan Szalontai <istvan.szalontai12@gmail.com>
2025-04-09 22:00:41 +0200

    solved added

--- js_tutorial/nongram/nonogram/main.lua
+++ js_tutorial/nongram/nonogram/main.lua
@@ -3,12 +3,33 @@ local current_level = {}
 
 function love.load()
     NONE = 1
-    BLACK = 2
+    FLAGGED = 2
     CROSS = 3
+
+    ACTIVATED = 2
+
+    MAX_DIGITS_IN_ROW = 8
+
+    EMPTY = 0
+    FILLED = 1
+
     GRID = 24
     MARGIN = 2
+
+    PICTURE = 1
+    DIGIT = 2
+
+    TITLE = 0
+    INGAME = 1
+    SOLVED = 2
+
     YELLOW = { 0.8, 0.8, 0.5, 0.5 }
     RED = { 0.8, 0, 0 }
+    LIGHTGREY = { 0.7, 0.7, 0.7 }
+    GREEN = { 0, 0.5, 0 }
+    BLACK = { 0.2, 0.2, 0.2 }
+
+
     grid = GRID + MARGIN
     title = "Nonogram"
     width, height = 1024, 600
@@ -24,10 +45,9 @@ function love.load()
     is_mouse_down = false
     mouse_state = NONE
     tiles = {}
-    load_level_by_id(6)
-    --counting rows
-
-    --counting columns
+    load_level_by_id(1000)
+    
+    game_state = INGAME
 end
 
 function load_level_by_id(id)
@@ -53,18 +73,19 @@ function get_level_by_id(id)
             return level
         end
     end
+    return levels[1]
 end
 
 function copy_pixels()
     for j = 0, current_level.height do
-        for i =0, current_level.width do
-            local r, g, b = imagedata:getPixel(i +  current_level.left, j + current_level.top)
-            local color = 0
+        for i = 0, current_level.width do
+            local r, g, b = imagedata:getPixel(i + current_level.left, j + current_level.top)
+            local color = EMPTY
             if r < 0.5 then
-                color = 1
+                color = FILLED
             end
             local tile = {
-                img = 1,
+                img = PICTURE,
                 value = color,
                 x = i,
                 y = j,
@@ -81,13 +102,13 @@ function counting_rows()
         local n = 0
         for i = 0, current_level.width do
             local tile = get_tile(i, j)
-            if tile.value == 1 then
+            if tile.value == FILLED then
                 counter = counter + 1
             else
                 if counter > 0 then
                     n = n + 1
                     local digit = {
-                        img = 2,
+                        img = DIGIT,
                         value = counter,
                         x = current_level.width + n,
                         y = j,
@@ -107,13 +128,13 @@ function counting_columns()
         local n = 0
         for j = current_level.height, 0, -1 do
             local tile = get_tile(i, j)
-            if tile.value == 1 then
+            if tile.value == FILLED then
                 counter = counter + 1
             else
                 if counter > 0 then
                     n = n + 1
                     local digit = {
-                        img = 2,
+                        img = DIGIT,
                         value = counter,
                         x = i,
                         y = -n,
@@ -125,6 +146,132 @@ function counting_columns()
             end
         end
     end
+end
+
+function get_digits_by_column(column)
+    local digits = {}
+    for row = -MAX_DIGITS_IN_ROW, 0 do
+        local tile = get_tile(column, row)
+        if tile ~= nil and tile.img == DIGIT then
+            table.insert(digits, tile)
+        end
+    end
+    return digits
+end
+
+function color_column(column)
+    local digits = get_digits_by_column(column)
+    local blocks = {}
+    local counter = 0
+    local n = 0
+    if #digits == 0 then
+        return
+    end
+    for row = 0, current_level.height do
+        local tile = get_tile(column, row)
+        
+        if tile.img == PICTURE and tile.state == FLAGGED then
+            counter = counter + 1
+        end
+
+        if counter > 0 and (tile.img == PICTURE and tile.state ~= FLAGGED or row ==  current_level.height) then
+            n = n + 1
+            local digit = {
+                value = counter,
+            }
+            table.insert(blocks, digit)
+            counter = 0
+        end
+        
+    end
+    local state = NONE
+    if #blocks == #digits then
+        local is_active = true
+        for i = 1, #digits do
+            if digits[i].value ~= blocks[i].value then
+                is_active = false
+            end
+        end
+        if is_active then
+            state = ACTIVATED
+        end
+    end
+    for i = 1, #digits do
+        digits[i].state = state
+    end
+end
+
+function get_digits_by_row(row)
+    local digits = {}
+    for column = current_level.width, current_level.width + MAX_DIGITS_IN_ROW do
+        local tile = get_tile(column, row)
+        if tile ~= nil and tile.img == DIGIT then
+            table.insert(digits, tile)
+        end
+    end
+    return digits
+end
+
+function color_row(row)
+    local digits = get_digits_by_row(row)
+    local blocks = {}
+    local counter = 0
+    local n = 0
+    if #digits == 0 then
+        return
+    end
+    for column = 0, current_level.width do
+        local tile = get_tile(column, row)
+
+        if tile.img == PICTURE and tile.state == FLAGGED then
+            counter = counter + 1
+        end
+
+        if counter > 0 and (tile.img == PICTURE and tile.state ~= FLAGGED or column ==  current_level.width) then
+            n = n + 1
+            local digit = {
+                value = counter,
+            }
+            table.insert(blocks, digit)
+            counter = 0
+        end
+        
+    end
+    local state = NONE
+    if #blocks == #digits then
+        local is_active = true
+        for i = 1, #digits do
+            if digits[i].value ~= blocks[i].value then
+                is_active = false
+            end
+        end
+        if is_active then
+            state = ACTIVATED
+        end
+    end
+    for i = 1, #digits do
+        digits[i].state = state
+    end
+end
+
+function color_digits(column, row)
+    color_row(row)
+    color_column(column)
+end
+
+function is_solved()
+    for i=1,#tiles do
+        local tile = tiles[i]
+        if tile.img == PICTURE then
+            if tile.value == FILLED and tile.state ~= FLAGGED then
+                return false
+            end
+            if tile.value == EMPTY and tile.state == FLAGGED then
+                return false
+            end
+        end
+    end
+    return true
 end
 
 function get_tile(x, y)
@@ -156,12 +303,12 @@ function love.draw()
         local tile = tiles[i]
         local x = tile.x * grid + current_level.offset_x
         local y = tile.y * grid + current_level.offset_y
-        if tile.img == 1 then
+        if tile.img == PICTURE then
             if tile.state == NONE then
-                love.graphics.setColor(0.5, 0.5, 0.5)
+                love.graphics.setColor(LIGHTGREY)
                 love.graphics.rectangle("fill", x, y, GRID, GRID)
-            elseif tile.state == BLACK then
-                love.graphics.setColor(0, 0, 0)
+            elseif tile.state == FLAGGED then
+                love.graphics.setColor(GREEN)
                 love.graphics.rectangle("fill", x, y, GRID, GRID)
             else
                 love.graphics.setColor(RED)
@@ -180,21 +327,36 @@ function love.draw()
                     end
                     if mouse_state == tile.state then
                         if tile.state == NONE then
-                            tile.state = BLACK
-                        elseif tile.state == BLACK then
+                            tile.state = FLAGGED
+                        elseif tile.state == FLAGGED then
                             tile.state = CROSS
                         else
                             tile.state = NONE
                         end
                     end
+                    color_digits(tile.x, tile.y)
+                    if is_solved() then
+                        game_state = SOLVED
+                    else
+                        game_state = INGAME
+                    end
                 else
                     is_mouse_down = false
                 end
             end
-        elseif tile.img == 2 then
-            love.graphics.setColor(0, 0, 0)
+        elseif tile.img == DIGIT then
+            local color = BLACK
+            if tile.state == ACTIVATED then
+                color = GREEN
+            end
+            love.graphics.setColor(color)
             love.graphics.print(tile.value, x, y)
         end
+    end
+
+    if game_state == SOLVED then
+        love.graphics.setColor(BLACK)
+        love.graphics.print("SOLVED", 20, 20)
     end
 end
 
